7f112e8885419056213a9e1ef0cceba50e1a46c9
Kripa <128496705+363958@users.noreply.github.com>
2026-06-07 22:08:01 +0545

    full update

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,12 +1,42 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.core.mail import send_mail
+
 
 def home(request):
 
     if request.method == "POST":
-        return JsonResponse({"status": "success"})
+
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        message = request.POST.get("message")
+
+        try:
+            send_mail(
+                subject=f"Portfolio Contact from {name}",
+                message=f"""
+Name: {name}
+
+Email: {email}
+
+Message:
+{message}
+""",
+                from_email=None,
+                recipient_list=["kripapdl12@gmail.com"],
+                fail_silently=False,
+            )
+
+            return JsonResponse({"status": "success"})
+
+        except Exception as e:
+            return JsonResponse({
+                "status": "error",
+                "message": str(e)
+            }, status=500)
 
     return render(request, "home.html")
+
 
 def cv_view(request):
     return render(request, "cv.html")
--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -110,5 +110,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'kripapdl12@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = os.environ.get('kripapdl12@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get("tvqz agzt jycv jvdn")
